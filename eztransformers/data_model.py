b5f6a68ff51dbbbb3e306f6019bc8f9504d647c1
Philip <p208p2002@gmail.com>
2019-12-22 01:52:54 +0000

    no message

---
+++ eztransformers/data_model.py
@@ -0,0 +1,9 @@
+class BertDataModel():
+    def __init__(self):
+        super().__init__()
+    def addInputAndLabel(self,input,label):
+        '''
+        input:str [CLS]input_str_a[SEP] or [CLS]input_str_a[SEP]input_str_b[SEP]
+        label:str
+        '''
+        pass
