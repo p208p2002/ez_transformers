8d1a42823c9889420e3256eafb35200e74453e38
Philip <p208p2002@gmail.com>
2020-08-06 02:31:48 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open('requirements.txt','r',encoding = 'utf-8') as f:
+    requirements = f.read().split("\n")
+
 setuptools.setup(
     name="ez_transformers",
     version="0.0.0",
@@ -7,4 +10,5 @@ setuptools.setup(
     author="Philip Huang",
     url="https://github.com/p208p2002/ez_transformers",
     packages= setuptools.find_packages(),
+    install_requires = requirements
     )
